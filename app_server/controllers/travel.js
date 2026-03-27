a92fbd785fd79931395f6fe25f6b60b8638cdc80
Ryan Grunest <ryan.grunest@chipotle.com>
2026-03-26 20:57:34 -0700

    Module 3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
 // GET travel page
 const travel = (req, res) => {
+  const trips = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../../data/trips.json"), "utf8"),
+  );
   res.render("travel", {
     title: "Travlr Getaways",
+    trips,
   });
 };
 
